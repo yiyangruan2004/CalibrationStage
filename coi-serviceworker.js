276266127b49cbbe07b960f30fdd9681f8de5fe9
Ruan2424 <Ruan2424@users.noreply.github.com>
2026-09-02 15:56:31 -0500

    demo

---
+++ coi-serviceworker.js
@@ -0,0 +1,28 @@
+const isolationHeaders = {
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Opener-Policy': 'same-origin'
+};
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+self.addEventListener('fetch', event => {
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+        return;
+
+    event.respondWith((async () => {
+        const response = await fetch(event.request);
+        if (response.type === 'opaque')
+            return response;
+
+        const headers = new Headers(response.headers);
+        for (const [name, value] of Object.entries(isolationHeaders))
+            headers.set(name, value);
+
+        return new Response(response.body, {
+            headers,
+            status: response.status,
+            statusText: response.statusText
+        });
+    })());
+});
